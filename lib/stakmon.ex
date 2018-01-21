4ce2f4f70049787e655234e1ef8451fff73b63cc
Cameron Currie <me@cameroncurrie.net>
2018-01-21 17:00:57 -0600

    Initial commit

---
+++ lib/stakmon.ex
@@ -0,0 +1,18 @@
+defmodule Stakmon do
+  @moduledoc """
+  Documentation for Stakmon.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Stakmon.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/stakmon_test.exs
@@ -0,0 +1,8 @@
+defmodule StakmonTest do
+  use ExUnit.Case
+  doctest Stakmon
+
+  test "greets the world" do
+    assert Stakmon.hello() == :world
+  end
+end
