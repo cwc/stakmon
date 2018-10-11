670b49d81cc0c6ad8388f0d5a5365735fe3a9ca5
Cameron Currie <me@cameroncurrie.net>
2018-10-10 19:37:04 -0500

    Report monthly electricity usage

--- lib/tplink_smartplugmon/plug_watcher.ex
+++ lib/tplink_smartplugmon/plug_watcher.ex
@@ -30,29 +30,49 @@ defmodule TplinkSmartplugmon.PlugWatcher do
   end
 
   defp poll(host, app_dir, poll_interval) do
-    {output, 0} = System.cmd("#{app_dir}/tplink_smartplug.py", ["-t", host, "-c", "energy"])
-    # Sent:      {"emeter":{"get_realtime":{}}}
-    # Received:  {"emeter":{"get_realtime":{"current":3.548681,"voltage":122.102035,"power":417.532206,"total":17.696000,"err_code":0}}}
 
-    [_, result] = String.split(output, "Received:", trim: true)
+    today = Date.utc_today
 
+    # Get plug info
     {output, 0} = System.cmd("#{app_dir}/tplink_smartplug.py", ["-t", host, "-c", "info"])
     [_, info] = String.split(output, "Received:", trim: true)
 
     info = Poison.decode!(info)
     plug_id = info["system"]["get_sysinfo"]["alias"]
 
-    Poison.decode!(result)
+    # Get usage data
+    {output, 0} = System.cmd("#{app_dir}/tplink_smartplug.py", ["-t", host, "-c", "energy"])
+    # Sent:      {"emeter":{"get_realtime":{}}}
+    # Received:  {"emeter":{"get_realtime":{"current":3.548681,"voltage":122.102035,"power":417.532206,"total":17.696000,"err_code":0}}}
+    
+    [_, result] = String.split(output, "Received:", trim: true)
+    realtime = Poison.decode!(result)
+
+    {output, 0} = System.cmd("#{app_dir}/tplink_smartplug.py", ["-t", host, "-j", ~s({"emeter":{"get_monthstat":{"year":#{today.year}}}})])
+    # Sent:      {"emeter":{"get_monthstat":{"year":2018}}}
+    # Received:  {"emeter":{"get_monthstat":{"month_list":[{"year":2018,"month":10,"energy":0.021000}],"err_code":0}}}
+
+    [_, result] = String.split(output, "Received:", trim: true)
+    monthstat = Poison.decode!(result)
+
+    # Send usage metrics
+    Map.merge(realtime["emeter"], monthstat["emeter"])
     |> post_report(plug_id)
 
     Process.send_after(self(), :poll, poll_interval)
   end
 
   defp post_report(report, plug_id) do
-    realtime = report["emeter"]["get_realtime"]
+    realtime = report["get_realtime"]
 
     Stakmon.Application.gauge("smartplug.current.amps", realtime["current"], tags: ["plug_id:#{plug_id}"])
     Stakmon.Application.gauge("smartplug.power.watts", realtime["power"], tags: ["plug_id:#{plug_id}"])
     Stakmon.Application.gauge("smartplug.total_usage", realtime["total"], tags: ["plug_id:#{plug_id}"])
+
+    month_list = report["get_monthstat"]["month_list"]
+
+    Enum.each(month_list, fn month ->
+      Stakmon.Application.gauge("smartplug.monthly_usage.kwh", month["energy"], tags: ["plug_id:#{plug_id}", "year:#{month["year"]}", "month:#{month["month"]}"])
+    end)
   end
 end
