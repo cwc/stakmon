506d1f5988527e8ff425dbb972a3936ca6e4fe46
Cameron Currie <me@cameroncurrie.net>
2019-03-27 00:49:12 -0500

    Add option to disable rig auto reset

--- lib/rigmon.ex
+++ lib/rigmon.ex
@@ -1,7 +1,7 @@
 defmodule Rigmon do
-  def start_rig_watcher({rig_id, _, _} = rig_spec) do
+  def start_rig_watcher(rig_spec) do
     Supervisor.start_child(Rigmon.Supervisor, %{
-      id: rig_id,
+      id: elem(rig_spec, 0),
       start: {Rigmon.RigWatcher, :start_link, [rig_spec]}
     })
   end
--- lib/rigmon/rig_watcher.ex
+++ lib/rigmon/rig_watcher.ex
@@ -4,12 +4,14 @@ defmodule Rigmon.RigWatcher do
   require Logger
 
   @reset_threshold_s 120
+  @power_on_delay_ms 200
 
   def start_link(rig_spec) do
     GenServer.start_link(__MODULE__, rig_spec)
   end
 
-  def init({rig_id, miner_spec, plug_spec}) do
+  def init(rig_spec = {_, _, _}), do: init(Tuple.append(rig_spec, []))
+  def init({rig_id, miner_spec, plug_spec, options}) do
     Logger.info("Monitoring rig #{rig_id}")
 
     {miner_mod, start_args} = miner_spec
@@ -36,18 +38,19 @@ defmodule Rigmon.RigWatcher do
       miner_pid: miner_pid,
       plug_mod: TplinkSmartplugmon,
       plug_pid: plug_pid,
-      last_reset: (DateTime.utc_now |> DateTime.to_unix) - 60,
+      last_reset: (DateTime.utc_now |> DateTime.to_unix) - @reset_threshold_s - 1,
+      auto_reset: options[:auto_reset],
     }}
   end
 
   def handle_cast(:miner_poll_error, state) do
     now = DateTime.utc_now |> DateTime.to_unix
 
-    if now - state.last_reset > @reset_threshold_s do
+    if state.auto_reset && now - state.last_reset > @reset_threshold_s do
       Logger.info(~s(Poll error for #{state.rig_id}: resetting rig))
 
       Kernel.apply(state.plug_mod, :power_off, [state.plug_pid])
-      Process.sleep(100)
+      Process.sleep(@power_on_delay_ms)
       Kernel.apply(state.plug_mod, :power_on, [state.plug_pid])
 
       {:noreply, Map.put(state, :last_reset, now)}
